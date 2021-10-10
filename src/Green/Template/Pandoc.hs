31381c686c8e5a1885a6113e257891679d70d51c
Logan McGrath <keywordsalad@thisfieldwas.green>
2021-10-10 04:35:46 -0700

    Reducing pandoc compiler to simiplest required code, and everything works!

--- src/Green/Template/Pandoc.hs
+++ src/Green/Template/Pandoc.hs
@@ -1,64 +1,21 @@
 module Green.Template.Pandoc where
 
-import Data.Bifunctor
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as HashMap
-import qualified Data.Text as T
-import Hakyll (Compiler, Item)
-import qualified Hakyll as H
+import Hakyll
 import System.FilePath
 import Text.Pandoc hiding (readers, writers)
 
-readers :: (PandocMonad m) => ReaderOptions -> HashMap String (String -> m Pandoc)
-readers opts =
-  HashMap.fromList $
-    second f
-      <$> [ (".md", readMarkdown),
-            (".markdown", readMarkdown)
-          ]
+compilePandoc :: Item String -> Compiler (Item String)
+compilePandoc item@(Item id' _) = do
+  let ext = takeExtension $ toFilePath id'
+  go ext
   where
-    f reader = reader opts . T.pack
-
-writers :: (PandocMonad m) => WriterOptions -> HashMap String (Pandoc -> m String)
-writers opts =
-  HashMap.fromList $
-    second f
-      <$> [ (".html", writeHtml5String)
-          ]
-  where
-    f writer = fmap T.unpack . writer opts
+    go ".html" = return item
+    go _ = do
+      pandoc <- readPandocWith readerOpts item
+      return $ writePandocWith writerOpts pandoc
 
 readerOpts :: ReaderOptions
-readerOpts =
-  let defOpts = H.defaultHakyllReaderOptions
-   in defOpts
-        { readerExtensions = enableExtension Ext_raw_html (readerExtensions defOpts)
-        }
+readerOpts = defaultHakyllReaderOptions
 
 writerOpts :: WriterOptions
-writerOpts =
-  let defOpts = H.defaultHakyllWriterOptions
-   in defOpts
-        { writerExtensions = enableExtension Ext_raw_html (writerExtensions defOpts)
-        }
-
-compilePandoc :: Item String -> Compiler (Item String)
-compilePandoc item =
-  let id' = H.itemIdentifier item
-      srcExt = takeExtension $ H.toFilePath id'
-   in go id' srcExt
-  where
-    go id' srcExt
-      | srcExt == ".html" = return item
-      | otherwise = do
-        let readerLookup = HashMap.lookup srcExt (readers def)
-        reader <- maybe (fail $ "Unrecognized read file extension " ++ show srcExt) return readerLookup
-
-        route <- H.getRoute id'
-        destExt <- maybe (fail $ "No route found for " ++ show id') (return . takeExtension) route
-        let writerLookup = HashMap.lookup destExt (writers writerOpts)
-        writer <- maybe (fail $ "Unrecognized write file extension " ++ show destExt) return writerLookup
-
-        case runPure . writer =<< runPure (reader $ H.itemBody item) of
-          Right x -> H.makeItem x
-          Left e -> fail $ show e
+writerOpts = defaultHakyllWriterOptions
