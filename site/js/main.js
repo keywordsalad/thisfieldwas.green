2d02c5165a07fe928b1f3c7f9200bcc857b9a816
Logan McGrath <keywordsalad@thisfieldwas.green>
2022-03-06 10:32:57 -0800

    Improving scrolling gradients

--- site/js/main.js
+++ site/js/main.js
@@ -1,12 +1,35 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
     writeCopyrightYear()
+    addCodeScrolling()
 })
 
 function writeCopyrightYear() {
-    let baseYear = '2012'
+    let baseYear = "2012"
     let year = new Date().getFullYear().toString()
     let copyrightDate = baseYear === year ? baseYear : `${baseYear}-${year}`
     Array.from(document.querySelectorAll(".copyright-date")).forEach(node => {
         node.innerText = copyrightDate
+    })
+}
+
+function addCodeScrolling() {
+    document.querySelectorAll("div.sourceCode").forEach(wrapper => {
+        let leftShadow = document.createElement("div")
+        leftShadow.classList.add("shadow", "shadow-left")
+        wrapper.appendChild(leftShadow)
+
+        let rightShadow = document.createElement("div")
+        rightShadow.classList.add("shadow", "shadow-right")
+        wrapper.appendChild(rightShadow)
+
+        let content = wrapper.querySelector("pre.sourceCode")
+        let contentScrollWidth = content.scrollWidth - wrapper.offsetWidth
+        function setScroll() {
+            let currentScroll = content.scrollLeft / contentScrollWidth
+            leftShadow.style.opacity = currentScroll
+            rightShadow.style.opacity = 1 - currentScroll
+        }
+        setScroll();
+        content.addEventListener("scroll", setScroll);
     })
 }
